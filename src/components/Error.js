2268f94addd27b37eb80f6cd378800761cd781a1
Dilli Luitel <dilliluitel@gmail.com>
2021-11-24 17:06:21 -0600

    Add error catch

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+    return (
+        <div>
+            <h1>Why did you break it ???</h1>
+        </div>
+    )
+}
+
+export default Error
--- src/components/uselessFacts.js
+++ src/components/uselessFacts.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import Error from "./Error";
 
 class uselessFacts extends Component {
   constructor(props) {
     super(props);
     this.state = {
       facts: [],
+      error: false,
     };
+  }
+  
+  uselessFacts() {
+    this.setState({ facts: [] }); 
   }
 
   componentDidMount() {
@@ -17,15 +23,25 @@ class uselessFacts extends Component {
       })
       .then((data) => {
         this.setState({ facts: data });
+      })
+      .catch((error) => {
+        this.setState({
+          error: true
+        })
       });
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.state.facts.text}</h1>
-      </div>
-    );
+    if(!this.state.error){
+      return (
+        <div>
+          <h1>{this.state.facts.text}</h1>
+          <button onClick={this.NewFact }>NewFact</button>
+        </div>
+      );
+  } else {
+    return <Error />
+  }
   }
 }
 
